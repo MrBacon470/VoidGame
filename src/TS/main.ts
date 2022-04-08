98eb978c2f4edb968a381e80f15f024ae3d19963
MrBacon470 <mrbacon470@outlook.com>
2022-04-08 08:43:58 -0500

    pushing changes data appears to not work

--- src/TS/main.ts
+++ src/TS/main.ts
@@ -1,8 +1,8 @@
 import { globalData as data } from "./Data";
-import { DOMCacheGetOrSet } from "./Internal/Cache.js"
+import { DOMCacheGetOrSet } from "./Cache"
 import Decimal from "break_eternity.js";
-import {format} from "./Internal/Formatting.js"
+import {format} from "./Formatting"
 
 console.log(data.void.toString());
-data.void = new Decimal(1);
+//data.void = new Decimal(1);
 DOMCacheGetOrSet("voidAmts").innerHTML = `${format(data.void)}`;
